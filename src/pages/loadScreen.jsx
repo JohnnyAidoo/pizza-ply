496433880e15425af1b89973cbd46d749d90297e
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-02-20 21:16:36 +0000

    'commit'

---
+++ src/pages/loadScreen.jsx
@@ -0,0 +1,13 @@
+import logo from '../images/pizzas/logo.png'
+
+
+function LoadScreen() {
+    
+    return (
+        <div className="LoadScreen">
+            <img src={logo}/>
+        </div>
+    );
+}
+
+export default LoadScreen;
